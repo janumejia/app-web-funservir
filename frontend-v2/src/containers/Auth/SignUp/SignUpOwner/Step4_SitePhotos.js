7f1b52ae1d1a60be23c6775f96cd0d3c5a09c31d
Andres Quintero <andres_f.quintero_s@uao.edu.co>
2023-04-09 13:01:10 -0500

    resolviendo conflictos nuevamente

--- frontend-v2/src/containers/Auth/SignUp/SignUpOwner/Step4_SitePhotos.js
+++ frontend-v2/src/containers/Auth/SignUp/SignUpOwner/Step4_SitePhotos.js
@@ -8,29 +8,7 @@ import FormControl from 'components/UI/FormControl/FormControl';
 import AddOwnerAction from './AddOwnerAction';
 import { FormHeader, Title, FormContent, FormAction } from './AddOwner.style';
 
-<<<<<<< HEAD
 const SitePhotos = ({ setStep }) => {
-=======
-
-
-
-// const handleChange = (info) => {
-
-//   const isJpgOrPng = info.file.type === 'image/jpeg' || info.file.type === 'image/png';
-//   if (!isJpgOrPng) {
-//     message.error('¡Solo puedes subir imagenes!');
-//   } else {
-//     getBase64(info.file);
-//   }
-
-//   /*const isLt2M = info.file.size / 1024 / 1024 < 2;
-//   if (!isLt2M) {
-//       message.error('La imagen debe ser menor a 2MB!');
-//   }*/
-// };
-
-const HotelPhotos = ({ setStep }) => {
->>>>>>> 5115f32 (cambios obligados)
   const { actions, state } = useStateMachine({ AddOwnerAction });
   const {
     register,
@@ -38,11 +16,7 @@ const HotelPhotos = ({ setStep }) => {
     setValue,
     handleSubmit,
   } = useForm({
-<<<<<<< HEAD
     defaultValues:  state?.data2?.sitePhotos 
-=======
-    defaultValues: state?.data,
->>>>>>> 5115f32 (cambios obligados)
   });
 
   useEffect(() => {
@@ -54,20 +28,6 @@ const HotelPhotos = ({ setStep }) => {
     setStep(5);
   };
   
-<<<<<<< HEAD
-
-  // const getBase64 = (img) => {
-  //   let base64Universal = [...state?.data2?.sitePhotos]
-  //   const reader = new FileReader();
-  //   if (img) {
-  //     reader.readAsDataURL(img);
-  //     reader.onloadend = () => {
-  //       actions.AddOwnerAction({ sitePhotos: reader.result });
-  //     }
-  //   }
-  // };
-=======
->>>>>>> fc852d1 (falta convertir a base64 y mandar al back)
 
   // const getBase64 = (img) => {
   //   let base64Universal = [...state?.data2?.sitePhotos]
@@ -81,6 +41,7 @@ const HotelPhotos = ({ setStep }) => {
   // };
 
   const getBase64 = (img) => {
+    let base64Universal = [...state?.data2?.hotelPhotos]
     const reader = new FileReader();
     if (img) {
       reader.readAsDataURL(img);
@@ -96,23 +57,6 @@ const HotelPhotos = ({ setStep }) => {
         <FormHeader>
           <Title>Paso 4 de 5: Fotos del sitio</Title>
         </FormHeader>
-<<<<<<< HEAD
-
-          <DragAndDropUploader
-<<<<<<< HEAD
-            name="sitePhotos"
-            value={state?.data2?.sitePhotos}
-            onUploadChange={(data2) => {
-              actions.AddOwnerAction({'sitePhotos': data2});
-              setValue('sitePhotos', data2)
-=======
-            name="hotelPhotos"
-            value={state?.data2?.hotelPhotos}
-            onUploadChange={(data2) => {
-              setValue('hotelPhotos', data2)
-
->>>>>>> 8f52dc9 (commit para poder actualizar desde main)
-=======
 
           <DragAndDropUploader
             name="sitePhotos"
@@ -120,7 +64,6 @@ const HotelPhotos = ({ setStep }) => {
             onUploadChange={(data2) => {
               actions.AddOwnerAction({'sitePhotos': data2});
               setValue('sitePhotos', data2)
->>>>>>> fc852d1 (falta convertir a base64 y mandar al back)
             }}
           />
 
