dda5a3456825184874ce7177f925de53049f6c86
Andres Quintero <andres_f.quintero_s@uao.edu.co>
2023-04-13 11:53:41 -0500

    arreglando lo que git dano

--- frontend-v2/src/context/AuthProvider.js
+++ frontend-v2/src/context/AuthProvider.js
@@ -35,7 +35,7 @@ const AuthProvider = (props) => {
             setUser({
               id: res.data.data._id,
               name: res.data.data.name,
-              avatar: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+              avatar: res.data.data.profilePicture,
               roles: res.data.data.userType,
             });
             setLoggedIn(true);
