7a92f3b02c27d0a80b89252ce092801dc9ba71c5
Julián Andrés Núñez <janumejia180599@gmail.com>
2023-06-21 16:02:45 -0500

    ultimo intento por hoy de la cookie

--- backend/src/controllers/controllersMainPage/authentication/login.js
+++ backend/src/controllers/controllersMainPage/authentication/login.js
@@ -49,7 +49,7 @@ const login = async (req, res) => {
                                 {
                                     httpOnly: true, // True = no puede ser accedida a traves de JavaScript
                                     secure: secureCookie, // True = solo puede se transportada por HTTPS, no http sin SSL
-                                    sameSite: "Lax", // La cookie solo podrá ser enviada al destino donde se genero lo cookie
+                                    sameSite: "None", // La cookie solo podrá ser enviada al destino donde se genero lo cookie
                                     // domain: matchedDomain,
                                     maxAge: 7 * 24 * 60 * 60 * 1000, // La cookie durará 7 días (en milisegundos) 
                                 })
