db5fbbe93b6cb18db6d6f010e1a5403dabc1267b
Julián Andrés Núñez <janumejia180599@gmail.com>
2023-08-12 15:46:59 -0500

    agregada la seccion de sitios mejor votados en la pantalla inicial

---
+++ backend/src/controllers/controllersMainPage/sites/getBestRankingSites.js
@@ -0,0 +1,22 @@
+require('dotenv').config({ path: '.env' })  // Para traer las variables de entorno
+const Site = require("../../../model/site") // Traemos el esquema del sitio
+
+const getBestRankingSites = async (req, res) => {
+    try{
+        // El select contiene los atributos a retornar de la consulta para cada documento de la colección
+        const dataFound = await Site.find({ status: "Aprobado" })
+            .populate("inclusiveElements")
+            .select("_id name category inclusiveElements location locality neighborhood gallery siteAddress status schedule rating ratingCount")
+            .sort({ rating: -1, ratingCount: -1 })
+            .limit(10); // Limit the results to 10 sites
+            
+        return res.json( dataFound )
+
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({})
+    }
+
+}
+
+module.exports = getBestRankingSites
