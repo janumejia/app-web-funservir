559a9774e15820b078f6d9f3fd230404563f6de6
Julián Andrés Núñez <janumejia180599@gmail.com>
2023-08-27 12:58:06 -0500

    ya se muestran alertas en los campos del formulario de creacion de comentario, al igual que se valida en el backend

--- backend/src/controllers/controllersMainPage/authentication/authenticatedUser/comments/addComment.js
+++ backend/src/controllers/controllersMainPage/authentication/authenticatedUser/comments/addComment.js
@@ -2,12 +2,66 @@ const Comment = require("../../../../../model/comments");
 const InclusiveSite = require("../../../../../model/site");
 const User = require("../../../../../model/user");
 const { ObjectId } = require('mongodb');
+const { ...regex } = require("../../../../../regex")
 
 const addComment = async (req, res) => {
 
-    const { ...inputs } = req.body;
-    const decodedDataInToken = req.decodedDataInToken;
     try {
+        const { ...inputs } = req.body;
+        const decodedDataInToken = req.decodedDataInToken;
+
+        // Cuando la entrada excede le limite permitido, el JSON de la petición llega vacío en este punto
+        if (Object.keys(inputs).length === 0) return res.status(413).json({ message: `El tamaño de la información enviada excede los límites permitidos.` });
+
+        const dataArray = [
+            { input: 'siteId', dataType: 'string', regex: regex._idMongooseRegex },
+            { input: 'title', dataType: 'string', regex: regex.siteNameRegex },
+            { input: 'content', dataType: 'string', regex: regex.descriptionRegex },
+        ]
+
+        // Función validateInput que toma tres argumentos: el valor del campo, el tipo de datos que se espera y la expresión regular que se utilizará para validar el valor.
+        // La función verifica si el valor del campo es válido según los criterios especificados y devuelve true o false.
+        const validateInput = (input, dataName, dataType, regex) => {
+            if (dataType === 'string') {
+                return typeof input === 'string' && regex.test(input);
+            }
+            if (dataType === 'array') {
+                return Array.isArray(input) && input.every(element => regex.test(element)); // Método every para iterar sobre cada uno de los elementos del arreglo y comprobar si cada elemento cumple con la expresión regular
+            }
+            if (dataType === 'object') {
+                return typeof input === 'object' && input !== null &&
+                    dataArray.every(({ input: requiredInput, properties, regex: requiredRegex }) => {
+                        if (requiredInput !== dataName) return true;
+                        return properties.every(prop => input.hasOwnProperty(prop) && requiredRegex.test(input[prop]));
+                    });
+            }
+            return false;
+        };
+
+        // El ciclo recorre cada elemento de la matriz dataArray y llama a validateInput con el valor correspondiente del campo del objeto JSON, el tipo de datos y la expresión regular.
+        // Si el valor del campo no es válido según los criterios especificados, se devuelve un mensaje de error.
+        for (const { input, dataType, regex } of dataArray) {
+            const inputValue = inputs[input];
+            if (!validateInput(inputValue, input, dataType, regex)) {
+                return res.status(422).json({ message: `El valor de ${input} no es válido` });
+            }
+        }
+
+        const validateStars = (arrayOfStars) => {
+            let isValid = true;
+
+            arrayOfStars.forEach(element => {
+                if (typeof element !== 'number' || !/^[1-5]$/.test(element.toString())) {
+                    isValid = false;
+                }
+            });
+
+            return isValid;
+        }
+
+        const resValidateStars = validateStars([inputs.stars, inputs.inclusivity, inputs.accessibility, inputs.service]);
+        if (!resValidateStars) return res.status(422).json({ message: `El valor de la puntuación en estrellas no es válido` });
+
         const newComment = new Comment({
             siteId: inputs.siteId,
             userId: ObjectId(decodedDataInToken._id),
@@ -29,7 +83,7 @@ const addComment = async (req, res) => {
                 },
             ]
         })
-       
+
         const savedComment = await newComment.save();
 
         await InclusiveSite.findOne({ _id: inputs.siteId }).then((site) => {
@@ -39,10 +93,10 @@ const addComment = async (req, res) => {
                 site.ratingCount = site.ratingCount + 1;
                 site.rating = (site.ratingTotal / site.ratingCount);
                 site.comments.push(savedComment._id);
-                
+
                 site.save();
                 return res.status(200).json({ message: "Comentario registrado", element: savedComment });
-            }else{
+            } else {
                 res.status(409).json({ message: "No existe el sitio solicitado" });
             }
         })
--- frontend-v2/src/containers/SinglePage/Review/ReviewForm.js
+++ frontend-v2/src/containers/SinglePage/Review/ReviewForm.js
@@ -29,7 +29,7 @@ import { Form, /*Label, GroupTitle, Description*/ } from './Review.style';
 // ];
 
 export default function ReviewForm(props) {
-  const {siteId, userId, close} = props;
+  const { siteId, userId, close } = props;
   const {
     control,
     formState: { errors },
@@ -41,11 +41,11 @@ export default function ReviewForm(props) {
     //   reviewPhotos,
     // },
   });
-  
+
   const onSubmit = async (data) => {
-    
+
     try {
-      const res = await axios.post(`${process.env.REACT_APP_HOST_BACK}/addComment`,  {...data, siteId, userId});
+      const res = await axios.post(`${process.env.REACT_APP_HOST_BACK}/addComment`, { ...data, siteId, userId });
       if (res) {
         if (res.status === 200) {
           close();
@@ -77,28 +77,48 @@ export default function ReviewForm(props) {
       <FormControl
         label="Calificación general"
         htmlFor="stars"
-        error={errors.ratings && <span>¡Este campo es requerido!</span>}
+        error={
+          errors.stars && errors.stars.type === "required" ? (
+            <span>¡Este campo es requerido!</span>
+          ) : errors.stars && errors.stars.type === "validate" ? (
+            <span>¡Este campo es requerido!</span>
+          ) : null
+        }
       >
         <Controller
           name="stars"
-          defaultValue=""
           control={control}
-          rules={{ required: true }}
+          rules={{
+            required: true,
+            validate: (value) => (/^[1-5]$/.test(parseInt(value)))
+          }}
           render={({ field: { onChange, onBlur, value } }) => (
-            <Rate onChange={onChange} onBlur={onBlur} value={value} />
+            <>
+
+              {console.log("value: ", value)}
+              <Rate onChange={onChange} onBlur={onBlur} value={value} />
+            </>
           )}
         />
       </FormControl>
       <FormControl
         label="Titulo de la opinión"
         htmlFor="title"
-        error={errors.reviewTitle && <span>¡Este campo es requerido!</span>}
+        error={
+          errors.title && errors.title.type === "required" ? (
+            <span>¡Este campo es requerido!</span>
+          ) : errors.title && errors.title.type === "pattern" ? (
+            <span>¡El valor ingresado no es válido!</span>
+          ) : null
+        }
       >
         <Controller
           name="title"
-          defaultValue=""
           control={control}
-          rules={{ required: true }}
+          rules={{
+            required: true,
+            pattern: /^([A-Za-z0-9ñÑáéíóúÁÉÍÓÚü\s,.:-]){1,255}$/,
+          }}
           render={({ field: { onChange, onBlur, value } }) => (
             <Input
               onChange={onChange}
@@ -112,13 +132,21 @@ export default function ReviewForm(props) {
       <FormControl
         label="Detalles de tu opinión"
         htmlFor="content"
-        error={errors.reviewDetails && <span>¡Este campo es requerido!</span>}
+        error={
+          errors.content && errors.content.type === "required" ? (
+            <span>¡Este campo es requerido!</span>
+          ) : errors.content && errors.content.type === "pattern" ? (
+            <span>¡El valor ingresado no es válido!</span>
+          ) : null
+        }
       >
         <Controller
           name="content"
-          defaultValue=""
           control={control}
-          rules={{ required: true }}
+          rules={{
+            required: true,
+            pattern: /^([A-Za-z0-9ñÑáéíóúÁÉÍÓÚü\s,.:\-;\(\)\[\]¿?¡!$&#\/]){1,2000}$/,
+          }}
           render={({ field: { onChange, onBlur, value } }) => (
             <Input.TextArea
               rows={5}
@@ -132,12 +160,24 @@ export default function ReviewForm(props) {
       </FormControl>
       <Row type="flex" justify="space-between">
         <Col>
-          <FormControl label="Elementos inclusivos" htmlFor="inclusivity">
+          <FormControl
+            label="Elementos inclusivos"
+            htmlFor="inclusivity"
+            error={
+              errors.inclusivity && errors.inclusivity.type === "required" ? (
+                <span>¡Este campo es requerido!</span>
+              ) : errors.inclusivity && errors.inclusivity.type === "validate" ? (
+                <span>¡Este campo es requerido!</span>
+              ) : null
+            }
+          >
             <Controller
               name="inclusivity"
-              defaultValue=""
               control={control}
-              rules={{ required: true }}
+              rules={{
+                required: true,
+                validate: (value) => (/^[1-5]$/.test(parseInt(value)))
+              }}
               render={({ field: { onChange, onBlur, value } }) => (
                 <Rate onChange={onChange} onBlur={onBlur} value={value} />
               )}
@@ -145,12 +185,24 @@ export default function ReviewForm(props) {
           </FormControl>
         </Col>
         <Col>
-          <FormControl label="Accesibilidad" htmlFor="accessibility">
+          <FormControl
+            label="Accesibilidad"
+            htmlFor="accessibility"
+            error={
+              errors.accessibility && errors.accessibility.type === "required" ? (
+                <span>¡Este campo es requerido!</span>
+              ) : errors.accessibility && errors.accessibility.type === "validate" ? (
+                <span>¡Este campo es requerido!</span>
+              ) : null
+            }
+          >
             <Controller
               name="accessibility"
-              defaultValue=""
               control={control}
-              rules={{ required: true }}
+              rules={{
+                required: true,
+                validate: (value) => (/^[1-5]$/.test(parseInt(value)))
+              }}
               render={({ field: { onChange, onBlur, value } }) => (
                 <Rate onChange={onChange} onBlur={onBlur} value={value} />
               )}
@@ -158,19 +210,31 @@ export default function ReviewForm(props) {
           </FormControl>
         </Col>
         <Col>
-          <FormControl label="Servicio" htmlFor="service">
+          <FormControl
+            label="Servicio"
+            htmlFor="service"
+            error={
+              errors.service && errors.service.type === "required" ? (
+                <span>¡Este campo es requerido!</span>
+              ) : errors.service && errors.service.type === "validate" ? (
+                <span>¡Este campo es requerido!</span>
+              ) : null
+            }
+          >
             <Controller
               name="service"
-              defaultValue=""
               control={control}
-              rules={{ required: true }}
+              rules={{
+                required: true,
+                validate: (value) => (/^[1-5]$/.test(parseInt(value)))
+              }}
               render={({ field: { onChange, onBlur, value } }) => (
                 <Rate onChange={onChange} onBlur={onBlur} value={value} />
               )}
             />
           </FormControl>
         </Col>
-        </Row>
+      </Row>
       {/* <FormControl
         label="What Sort of trip was this?"
         htmlFor="tripType"
