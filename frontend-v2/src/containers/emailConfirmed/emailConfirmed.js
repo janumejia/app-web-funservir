5bd7b02dbd260ee1f495990c5fcb01b19081c192
Andres Quintero <andres_f.quintero_s@uao.edu.co>
2023-08-31 00:42:47 -0500

    cuando el token es invalido muestra la 404

--- frontend-v2/src/containers/emailConfirmed/emailConfirmed.js
+++ frontend-v2/src/containers/emailConfirmed/emailConfirmed.js
@@ -1,41 +1,52 @@
-import React, {useEffect} from 'react';
+import { message, Alert } from 'antd';
+import React, { useEffect, useState } from 'react';
 import Image from 'components/UI/Image/Image';
 import TextLink from 'components/UI/TextLink/TextLink';
 import EmailConfirmedWrapper, { ContentWrapper } from './emailConfirmed.style';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from "../../settings/axiosConfig";
 
-const EmailConfirmed = ({ staticContext = {} }) => {
+const EmailConfirmed = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
   const queryParams = new URLSearchParams(location.search);
 
   const token = queryParams.get('token');
   const email = queryParams.get('email');
   const expires = queryParams.get('expires');
+
   useEffect(() => {
     const validate = async () => {
-      const res = await axios.post(`${process.env.REACT_APP_HOST_BACK}/confirmEmailToken`, { token, email, expires });
-      if(!res){
-        staticContext.status = 404;
-      }
+      await axios.post(`${process.env.REACT_APP_HOST_BACK}/confirmEmailToken`, { token, email, expires }).then((res) => {
+        if (res.data.message === "Error") {
+          navigate(`/token=${token}&email=${email}&expires=${expires}`, { replace: true });
+          message.error('El link ha expirado o es inválido');
+        }
+        setIsLoading(false);
+      });
     }
-
     try {
       validate();
-      
     } catch (error) {
-
+      message.error('Ha ocurrido un error');
     }
+
   })
 
 
   return (
-    <EmailConfirmedWrapper>
-      <ContentWrapper>
-        <Image src="/images/confirmed.png" alt="email confirmado" />
-        <TextLink link="/" content="Volver" />
-      </ContentWrapper>
-    </EmailConfirmedWrapper>
+    (isLoading) ?
+      <></>
+      :
+      (
+        <EmailConfirmedWrapper>
+          <ContentWrapper>
+            <Image src="/images/confirmed.png" alt="email confirmado" />
+            <TextLink link="/" content="Volver" />
+          </ContentWrapper>
+        </EmailConfirmedWrapper>
+      )
   );
 };
 
